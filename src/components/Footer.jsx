c16f5c50b166cb3f6c7e1713168910c74dd1cdbf
Matt Haynes <merkabafox@gmail.com>
2023-10-07 10:54:42 -0400

    added icons with links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ class Footer extends Component {
       <>
         <footer className="fixed-bottom bg-secondary">
           <div className="d-flex justify-content-center flex-wrap">
-            <a className="p-4">GitHub</a>
-            <a className="p-4">LinkedIn</a>
-            <a className="p-4">StackOverflow</a>
+            <a className="fa fa-github p-4" style={{ color: "white", fontSize: 22 }} href="https://github.com/iggy-ziggy"></a>
+            <a className="fa fa-linkedin p-4" style={{ color: "white", fontSize: 22 }} href="https://www.linkedin.com/"></a>
+            <a className="fa fa-stack-overflow p-4" style={{ color: "white", fontSize: 22 }} href="https://stackoverflow.com/"></a>
           </div>
         </footer>
       </>
