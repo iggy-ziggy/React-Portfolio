e2a76afdf5cac065a360abc9ec6b885cdf0fe237
Matt Haynes <merkabafox@gmail.com>
2023-10-11 12:24:50 -0400

    added downloadable resume

---
+++ src/components/pages/Skills.js
@@ -0,0 +1,38 @@
+const skills = [
+  {
+    name: "HTML",
+  },
+  {
+    name: "CSS",
+  },
+  {
+    name: "JavaScript",
+  },
+  {
+    name: "Bootstrap",
+  },
+  {
+    name: "Handlebars",
+  },
+  {
+    name: "React",
+  },
+  {
+    name: "Node",
+  },
+  {
+    name: "SQL",
+  },
+  {
+    name: "MongoDB",
+  },
+  {
+    name: "Express",
+  },
+  {
+    name: "PWA",
+  },
+];
+
+
+export default skills;
