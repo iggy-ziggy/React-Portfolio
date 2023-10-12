86d1a0d8c8d05caa805dbec4d2e1208cedd094c8
Matt Haynes <merkabafox@gmail.com>
2023-10-12 17:45:45 -0400

    better images

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,13 +2,13 @@
 const projects = [
     {
         title: 'Random Password Generator',
-        image: 'https://collegeprepconsultant.com/wp-content/uploads/2016/12/placeholder-500x500.jpg',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlFsuqwQqlh_V7M4GtMt2pOohllKcmcldxtw&usqp=CAU',
         github: 'https://github.com/iggy-ziggy/random-password-generator',
         deploy: 'https://iggy-ziggy.github.io/random-password-generator/'
     },
     {
         title: 'Code Quiz',
-        image: 'https://collegeprepconsultant.com/wp-content/uploads/2016/12/placeholder-500x500.jpg',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTarC4vD5m5YB-p8hwxVjvAxkvjy734WaoAQ&usqp=CAU',
         github: 'https://github.com/iggy-ziggy/code_quiz',
         deploy: 'https://iggy-ziggy.github.io/code_quiz/'
     },
@@ -26,25 +26,25 @@ const projects = [
     // },
     {
         title: 'Power Grid',
-        image: 'https://collegeprepconsultant.com/wp-content/uploads/2016/12/placeholder-500x500.jpg',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlAywbtoaGH14yP-QqXZioWv9z2W1NGYZb5rkUObt6Gob-kGxqF1YcKrhxT0_cj4beRgc&usqp=CAU',
         github: 'https://github.com/larsly/PowerGrid',
         deploy: 'https://larsly.github.io/PowerGrid/'
     },
     {
         title: 'Social Network API',
-        image: 'https://collegeprepconsultant.com/wp-content/uploads/2016/12/placeholder-500x500.jpg',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaudG84aX7-GBXc7PBqIk5yf9pDRly1TIaRg&usqp=CAU',
         github: 'https://github.com/iggy-ziggy/Social-Network-API',
         deploy: 'https://watch.screencastify.com/v/IUmMRwbsLSbKGZWOCFeT'
     },
     {
         title: 'E-Commerce Backend',
-        image: 'https://collegeprepconsultant.com/wp-content/uploads/2016/12/placeholder-500x500.jpg',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0gxYXNEwcT1FebDuMrGjRkBCfQ56pWcwrhg&usqp=CAU',
         github: 'https://github.com/iggy-ziggy/e-commerce-back-end',
         deploy: 'https://watch.screencastify.com/v/ugHBaT2o8CNyXhsXe0q0'
     },
     {
         title: 'Workday Scheduler',
-        image: 'https://collegeprepconsultant.com/wp-content/uploads/2016/12/placeholder-500x500.jpg',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO_tvx7tgR4QkJK9vs3y3itY-TuUHPjRPNdw&usqp=CAU',
         github: 'https://github.com/iggy-ziggy/work-day-scheduler',
         deploy: 'https://iggy-ziggy.github.io/work-day-scheduler/'
     }
